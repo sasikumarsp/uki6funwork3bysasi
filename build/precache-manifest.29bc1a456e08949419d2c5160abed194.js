556623175a26d3c3d8a1c377a695832d248bb31b
sasikumarsp <sasisp2000@gmail.com>
2020-05-12 12:33:54 +0530

    make it better

---
+++ build/precache-manifest.29bc1a456e08949419d2c5160abed194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1bfd9ba28ec4f678b4bd5f14b7ad2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e7854c7f4eec340224",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c301bc5028c7df644770",
+    "url": "/static/js/2.2b752fa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b752fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e7854c7f4eec340224",
+    "url": "/static/js/main.3333d776.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
