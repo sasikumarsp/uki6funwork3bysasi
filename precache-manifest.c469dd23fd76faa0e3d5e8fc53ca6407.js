38ca7ba1483022aaf416d6aee495da7836e73d90
sasikumarsp <sasisp2000@gmail.com>
2020-05-12 14:28:48 +0530

    Updates

---
+++ precache-manifest.c469dd23fd76faa0e3d5e8fc53ca6407.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533e0671c9bc1b6c1ff331f8ddf43611",
+    "url": "/sasikumarsp/uki6funwork3bysasi/index.html"
+  },
+  {
+    "revision": "3ea63e44547d302191f4",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/css/main.d1786e62.chunk.css"
+  },
+  {
+    "revision": "bc1a1096ffd6c74763d2",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/js/2.33ea0943.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/js/2.33ea0943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea63e44547d302191f4",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/js/main.2a1bb790.chunk.js"
+  },
+  {
+    "revision": "95f32b49605331a1b45a",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/js/runtime-main.5bcd1100.js"
+  },
+  {
+    "revision": "2bad2a4f09b24b652f54adc784639375",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/media/Deathblood3D-YpV4.2bad2a4f.woff"
+  },
+  {
+    "revision": "fdaeb02653d75419010fc7bb4f491f07",
+    "url": "/sasikumarsp/uki6funwork3bysasi/static/media/DeathbloodAcademy-MgBn.fdaeb026.woff"
+  }
+]);
